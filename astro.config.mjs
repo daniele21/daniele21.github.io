9cb62484970ba48b58018e4cf68a849141532444
daniele21 <danielemoltisanti@live.it>
2026-08-05 23:32:58 +0200

    ci: add GitHub Actions workflow for deployment previews and update Astro config to support dynamic base paths

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://daniele21.github.io',
+  base: process.env.ASTRO_BASE || '/',
   build: {
     assets: '_assets',
   },
