372cd7395a7199450c933852b550250cf36d95fa
daniele21 <danielemoltisanti@live.it>
2026-08-09 16:57:00 +0200

    seo and geo improvements

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://daniele21.github.io',
   base: process.env.ASTRO_BASE || '/',
+  integrations: [sitemap()],
   build: {
     assets: 'assets',
   },
